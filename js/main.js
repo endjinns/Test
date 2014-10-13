0441ce679bbbee811efdc439930eefe5f9bbf094
endjinns <andrew.william.cowan@gmail.com>
2014-10-13 14:47:19 +0100

    More changes.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,5 @@
 var app = {
 
-    renderHomeView: function() {
-        $('body').html(this.homeTpl());
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-
-
-    findByName: function () {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function (employees) {
-            $('.employee-list').html(self.employeeLiTpl(employees));
-        });
-    },
 
     showAlert: function (message, title) {
         if (navigator.notification) {
@@ -20,19 +8,13 @@ var app = {
             alert(title ? (title + ": " + message) : message);
         }
     },
-    initialize: function() {
+    initialize: function () {
         var self = this;
-        this.homeTpl = Handlebars.compile($("#home-tpl").html());
-        this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-        this.store = new MemoryStore(function() {
-            self.renderHomeView();
+        this.store = new MemoryStore(function () {
+            $('body').html(new HomeView(self.store).render().el);
         });
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
-    //initialize: function() {
-    //    this.store = new MemoryStore();
-    //    $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    //}
+
 
 };
 
